0773843ee3c163b430fcc974f1abdd88659c57c6
Rafael Monteiro <rafajapones@gmail.com>
2017-07-05 22:32:02 -0300

    Montando combos dinamicamente.

--- src/main/java/com/cobranca/controller/TitulosController.java
+++ src/main/java/com/cobranca/controller/TitulosController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.cobranca.model.StatusTitulo;
 import com.cobranca.model.Titulo;
 import com.cobranca.repository.Titulos;
 
@@ -19,8 +20,10 @@ public class TitulosController {
 	private Titulos titulos;
 	
 	@GetMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatus", StatusTitulo.values());
+		return mv;
 	}
 	
 	@PostMapping
